b2543613e165bbfeddd6eeeadc145156f07bef4b
Nicolas Lizama <Lmatinico@gmail.com>
2025-03-07 20:07:37 -0300

    capturar valor entrada

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let nombres = ["Juan", "María", "Pedro", "Ana"];
+let amigos = [];
 
+//función para agregar amigos
+
+function agregarAmigo(){
+    let input = document.getElementById('amigo').value;
+    console.log(input)
+        return;
+}
